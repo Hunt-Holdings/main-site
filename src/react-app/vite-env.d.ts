1d4b271a342aa8ead684d2b58f1cdd8f0c7f3065
Carson Hunt <carjhunt3@gmail.com>
2026-02-04 09:47:38 -0600

    reworked

--- src/react-app/vite-env.d.ts
+++ src/react-app/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_COMPANY_NAME: string;
+  readonly VITE_PHONE_NUMBER: string;
+  readonly VITE_EMAIL: string;
+  readonly VITE_LINKEDIN_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
