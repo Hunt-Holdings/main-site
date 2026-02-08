3a3b26a90dc6ad655c718f0607652717e1895930
Carson740 <61304405+Carson740@users.noreply.github.com>
2026-02-07 23:06:37 -0600

    update route path for terms and conditions

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Menu from "./pages/legal/Menu.tsx";
-import Terms from "./pages/legal/TermsAndConditions.tsx";
+import TermsAndConditions from "./pages/legal/TermsAndConditions.tsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/legal" element={<Menu />} />
-      <Route path="/legal/terms" element={<Terms />} />
+      <Route
+        path="/legal/termsandconditions"
+        element={<TermsAndConditions />}
+      />
     </Routes>
   );
 }
